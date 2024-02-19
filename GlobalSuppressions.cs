0c179e7b25053bf75c53bb03dd6833c40c0fea05
Peter Voigt <156658983+NcPeter@users.noreply.github.com>
2024-02-19 18:16:41 +0100

    Add files via upload

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0059:Unnötige Zuweisung eines Werts.", Justification = "<Ausstehend>", Scope = "member", Target = "~M:NC_Tool.Form2.Fraesbahnausgabe_Z")]
+[assembly: SuppressMessage("Style", "IDE0059:Unnötige Zuweisung eines Werts.", Justification = "<Ausstehend>", Scope = "member", Target = "~M:NC_Tool.Form2.Fraesbahnausgabe_Y")]
+[assembly: SuppressMessage("Style", "IDE0059:Unnötige Zuweisung eines Werts.", Justification = "<Ausstehend>", Scope = "member", Target = "~M:NC_Tool.Form2.Fraesbahnausgabe_X")]
+[assembly: SuppressMessage("Style", "IDE0059:Unnötige Zuweisung eines Werts.", Justification = "<Ausstehend>", Scope = "member", Target = "~M:NC_Tool.Form2.PrintDocument1_PrintPage(System.Object,System.Drawing.Printing.PrintPageEventArgs)")]
